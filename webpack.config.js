6e2c78f59e675453826600cd2566e2b0ff5d6342
Thebestharrison1221 <100962097+Thebestharrison1221@users.noreply.github.com>
2025-01-01 14:42:01 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
-// webpack.config.js
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', // Path to your entry file
+  entry: './src/index.js', // Entry point for JavaScript
   output: {
     path: path.resolve(__dirname, 'dist'), // Output directory
-    filename: 'bundle.js' // Output file name
+    filename: 'bundle.js', // Output file
   },
-  mode: 'production' // Or 'development' depending on your environment
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // Root index.html file
+    }),
+  ],
 };
